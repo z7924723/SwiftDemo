c61c66411ce65606cc8b8aeac0a8811ff38dcb99
PinguMac <PinguMac@Mac.local>
2017-12-10 02:14:59 +0800

    Upload demo video

--- SeekBarPercentage/SeekBarPercentage/ViewController.swift
+++ SeekBarPercentage/SeekBarPercentage/ViewController.swift
@@ -54,23 +54,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
     self.view.endEditing(true)
   }
   
-//  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//    // Create an `NSCharacterSet` set which includes everything *but* the digits
-//    let inverseSet = NSCharacterSet(charactersIn:"0123456789").inverted
-//
-//    // At every character in this "inverseSet" contained in the string,
-//    // split the string up into components which exclude the characters
-//    // in this inverse set
-//    let components = string.components(separatedBy: inverseSet)
-//
-//    // Rejoin these components
-//    let filtered = components.joined(separator: "")  // use join("", components) if you are using Swift 1.2
-//
-//    // If the original string is equal to the filtered string, i.e. if no
-//    // inverse characters were present to be eliminated, the input is valid
-//    // and the statement returns true; else it returns false
-//    return string == filtered
-//  }
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    // Create an `NSCharacterSet` set which includes everything *but* the digits
+    let inverseSet = NSCharacterSet(charactersIn:"0123456789").inverted
+
+    // At every character in this "inverseSet" contained in the string,
+    // split the string up into components which exclude the characters
+    // in this inverse set
+    let components = string.components(separatedBy: inverseSet)
+
+    // Rejoin these components
+    let filtered = components.joined(separator: "")  // use join("", components) if you are using Swift 1.2
+
+    // If the original string is equal to the filtered string, i.e. if no
+    // inverse characters were present to be eliminated, the input is valid
+    // and the statement returns true; else it returns false
+    return string == filtered
+  }
   
   @IBAction func UpdateDataBySlider(_ sender: UISlider) {
     if let number = Int(UserInputText.text!) {
