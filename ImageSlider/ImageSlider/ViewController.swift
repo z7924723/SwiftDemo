a4670de72257395b2902caf00a9fd83d19587d39
PinguMac <PinguMac@Mac.local>
2017-12-12 17:01:35 +0800

    Add implement code and demo git file

--- ImageSlider/ImageSlider/ViewController.swift
+++ ImageSlider/ImageSlider/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController, UIScrollViewDelegate {
   var scrollView: UIScrollView!
   var pageControl: UIPageControl!
   var fullSize: CGSize!
+  var index: Int!
+  
+  var imgList: [UIImage] = [
+    UIImage(named: "01.jpg")!,
+    UIImage(named: "02.jpg")!,
+    UIImage(named: "03.jpg")!,
+    UIImage(named: "04.jpg")!,
+    UIImage(named: "05.jpg")!
+  ]
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,8 +35,9 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     // Scroll View
     scrollView = UIScrollView()
     scrollView.frame = CGRect(x: 0, y: 20, width: fullSize.width, height: fullSize.height - 20)
-    // 實際視圖範圍
-    scrollView.contentSize = CGSize(width: fullSize.width * 5, height: fullSize.height - 20)
+    // 實際視圖範圍,有幾張圖就為幾倍
+    scrollView.contentSize = CGSize(width: fullSize.width * CGFloat(imgList.count),
+                                    height: fullSize.height)
 
     // 是否顯示滑動條
     scrollView.showsHorizontalScrollIndicator = false
@@ -44,9 +54,9 @@ class ViewController: UIViewController, UIScrollViewDelegate {
                                               y: 0,
                                               width: fullSize.width * 0.85,
                                               height: 50))
-    pageControl.center = CGPoint(x: fullSize.width * 0.5, y: fullSize.height * 0.85)
-    pageControl.numberOfPages = pageNum
-    pageControl.currentPage = 0
+    pageControl.center = CGPoint(x: fullSize.width * 0.5, y: fullSize.height * 0.9)
+    pageControl.numberOfPages = imgList.count
+    pageControl.currentPage = 0 // starting page
     
     pageControl.currentPageIndicatorTintColor = UIColor.black // 目前所在頁數的點點顏色
     pageControl.pageIndicatorTintColor = UIColor.lightGray // 其餘頁數的點點顏色
@@ -57,16 +67,13 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     self.view.addSubview(pageControl)
     
-    
-    var myLabel = UILabel()
-    for i in 0...4 {
-      myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: fullSize.width, height: 40))
-      myLabel.center = CGPoint(x: fullSize.width * (0.5 + CGFloat(i)),
-                               y: fullSize.height * 0.2)
-      myLabel.font = UIFont(name: "Helvetica-Light", size: 48.0)
-      myLabel.textAlignment = .center
-      myLabel.text = "\(i + 1)"
-      scrollView.addSubview(myLabel)
+    for index in 0...imgList.count - 1 {
+      let imgView = UIImageView(image: imgList[index])
+      imgView.frame = CGRect(x: 0, y: 0, width: fullSize.width, height: 500)
+      imgView.center = CGPoint(x: fullSize.width * (0.5 + CGFloat(index)),
+                               y: fullSize.height * 0.5)
+      
+      scrollView.addSubview(imgView)
     }
   }
 
@@ -76,7 +83,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
   }
 
   func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-    // 左右滑動到新頁時 更新 UIPageControl 顯示的頁數
+    // Update page number when user slide
     let page = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
     pageControl.currentPage = page
   }
