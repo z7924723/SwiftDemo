dfdb3d2cb4faa9404217b4d7ad4298fe6c2b0015
PinguMac <PinguMac@Mac.local>
2018-01-10 10:36:50 +0800

    A demo for shake detect

---
+++ SharkDetect/ShakeDetect/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  SharkDetect
+//
+//  Created by PinguMac on 2018/1/10.
+//  Copyright © 2018年 PinguMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  let imgArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg"]
+  
+  let imgView: UIImageView = {
+    let imageView = UIImageView()
+    return imageView
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    let ScreenSize = UIScreen.main.bounds.size
+    self.view.backgroundColor = UIColor.white
+    
+    imgView.image = UIImage(named: imgArray[Int(arc4random_uniform(6))])
+    imgView.frame = CGRect(x: 0,
+                           y: 0,
+                           width: ScreenSize.width,
+                           height: ScreenSize.height / 2)
+    imgView.center = CGPoint(x: ScreenSize.width * 0.5,
+                             y: ScreenSize.height * 0.5)
+    self.view.addSubview(imgView)
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+    updateImage()
+  }
+  
+  func updateImage() {
+    var randIndex = Int(arc4random_uniform(6))
+    imgView.image = UIImage(named: imgArray[randIndex])
+  }
+}
+
